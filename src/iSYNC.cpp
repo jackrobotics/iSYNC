e76503532ce53ceaa7a1931e7a5957b825ae12a4
Sonthaya Boonchan <sonthaya.jackrobotic@mail.kmutt.ac.th>
2019-03-24 14:57:50 +0700

    Add files via upload

--- src/iSYNC.cpp
+++ src/iSYNC.cpp
@@ -1,124 +1,220 @@
 #include "iSYNC.h"
-iSYNC::iSYNC(){
+iSYNC::iSYNC(Client& netclient)
+{
+    clientSecure = &netclient;
     debug = false;
     httpRuning = false;
 }
-void iSYNC::debugMode(bool _debug = true){
+void iSYNC::debugMode(bool _debug = true)
+{
     debug = _debug;
 }
-void iSYNC::begin(String ssid,String password){
-    if(debug)Serial.print("Connect to SSID: ");
-    if(debug)Serial.println(ssid);
+void iSYNC::begin(String ssid, String password)
+{
+#if defined(ESP8266) || defined(ESP32)
+    if (debug)
+        Serial.print("Connect to SSID: ");
+    if (debug)
+        Serial.println(ssid);
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid.c_str(), password.c_str());
-    while (WiFi.status() != WL_CONNECTED) {
-        if(debug)Serial.print(".");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        if (debug)
+            Serial.print(".");
         // wait 1 second for re-trying
         delay(100);
     }
-    if(debug)Serial.print("Connected to ");
-    if(debug)Serial.println(ssid);
+    if (debug)
+        Serial.print("Connected to ");
+    if (debug)
+        Serial.println(ssid);
+#endif
 }
 
-String iSYNC::HTTP_GET_RAW(String key){
+String iSYNC::HTTP_GET_RAW(String key, String auth)
+{
     String inMSG = "";
-    if(httpRuning){
-        if(debug)Serial.println("\nHTTP Runing... please wait!");
+    if (httpRuning)
+    {
+        if (debug)
+            Serial.println("\nHTTP Runing... please wait!");
         return "";
     }
     httpRuning = true;
-#if defined(ESP8266)
-    const char FINGERPRINT[] = "fc 72 ae 00 0d ba cd b0 c8 00 97 74 39 79 21 36 64 e9 79 9f";
-    clientSecure.setFingerprint(FINGERPRINT);
-#endif
-    if(debug)Serial.print("\nStarting connection to server...");
-    
-    if (!clientSecure.connect(SERVER_API, 443)){
-        if(debug)Serial.println("Connection failed!");
-    }else {
-        if(debug)Serial.println("Connected to server!");
-        String _host = "Host: ";_host+=SERVER_API;
-        // Make a HTTP request:
-        clientSecure.println("GET https://www.isync.pro/data/"+key+" HTTP/1.0");
-        clientSecure.println(_host);
-        clientSecure.println("Connection: close");
-        clientSecure.println();
+    if (debug)
+        Serial.print("\nStarting connection to server...");
 
-        while (clientSecure.connected()) {
-            String line = clientSecure.readStringUntil('\n');
-            if (line == "\r") break;
+    if (!clientSecure->connect(SERVER_API, 443))
+    {
+        if (debug)
+            Serial.println("Connection failed!");
+    }
+    else
+    {
+        if (debug)
+            Serial.println("Connected to server!");
+        String _host = "Host: ";
+        _host += SERVER_API;
+        // Make a HTTP request:
+        clientSecure->println("GET https://api.isync.pro/data/" + key + "?auth=" + auth + " HTTP/1.0");
+        clientSecure->println(_host);
+        clientSecure->println("Connection: close");
+        clientSecure->println();
+
+        while (clientSecure->connected())
+        {
+            String line = clientSecure->readStringUntil('\n');
+            if (line == "\r")
+                break;
         }
 
         // if there are incoming bytes available
-        while (clientSecure.available()) {
-            char c = clientSecure.read();
+        while (clientSecure->available())
+        {
+            char c = clientSecure->read();
             inMSG += c;
         }
-        if(debug){
+        if (debug)
+        {
             Serial.println("Received Data ::.");
             Serial.println(inMSG);
         }
-        clientSecure.stop();
+        clientSecure->stop();
     }
     httpRuning = false;
     return inMSG;
 }
 
-String iSYNC::HTTP_POST(String key,String msg){
+String iSYNC::HTTP_POST(String key, String auth, String msg)
+{
     String inMSG = "";
-    if(httpRuning){
-        if(debug)Serial.println("\nHTTP Runing... please wait!");
+    if (httpRuning)
+    {
+        if (debug)
+            Serial.println("\nHTTP Runing... please wait!");
         return "";
     }
     httpRuning = true;
-#if defined(ESP8266)
-    const char fingerprint[] = "fc 72 ae 00 0d ba cd b0 c8 00 97 74 39 79 21 36 64 e9 79 9f";
-    clientSecure.setFingerprint(fingerprint);
-#endif
-    if(debug)Serial.print("\nStarting connection to server...");
-    
-    if (!clientSecure.connect(SERVER_API, 443)){
-        if(debug)Serial.println("Connection failed!");
-    }else {
-        if(debug)Serial.println("Connected to server!");
-        String _host = "Host: ";_host+=SERVER_API;
-        // Make a HTTP request:
-        clientSecure.println("GET https://www.isync.pro/data/"+key+"/set/"+msg+" HTTP/1.0");
-        clientSecure.println(_host);
-        clientSecure.println("Connection: close");
-        clientSecure.println();
+    if (debug)
+        Serial.print("\nStarting connection to server...");
 
-        while (clientSecure.connected()) {
-            String line = clientSecure.readStringUntil('\n');
-            if (line == "\r") break;
+    if (!clientSecure->connect(SERVER_API, 443))
+    {
+        if (debug)
+            Serial.println("Connection failed!");
+    }
+    else
+    {
+        if (debug)
+            Serial.println("Connected to server!");
+        String _host = "Host: ";
+        _host += SERVER_API;
+        // Make a HTTP request:
+        clientSecure->println("GET https://api.isync.pro/data/" + key + "/set/" + msg + "?auth=" + auth + " HTTP/1.0");
+        clientSecure->println(_host);
+        clientSecure->println("Connection: close");
+        clientSecure->println();
+
+        while (clientSecure->connected())
+        {
+            String line = clientSecure->readStringUntil('\n');
+            if (line == "\r")
+                break;
         }
 
         // if there are incoming bytes available
-        while (clientSecure.available()) {
-            char c = clientSecure.read();
+        while (clientSecure->available())
+        {
+            char c = clientSecure->read();
             inMSG += c;
         }
-        if(debug){
+        if (debug)
+        {
             Serial.println("Send Data ::.");
             Serial.println(inMSG);
         }
-        clientSecure.stop();
+        clientSecure->stop();
     }
     httpRuning = false;
     return inMSG;
 }
 
-String iSYNC::HTTP_GET(String key){
+String iSYNC::HTTP_GET(String key, String auth)
+{
     StaticJsonDocument<200> obj;
-    String json = HTTP_GET_RAW(key);
+    String json = HTTP_GET_RAW(key, auth);
     DeserializationError error = deserializeJson(obj, json);
     return obj[0];
 }
 
-String iSYNC::getPayload(String json){
+String iSYNC::getPayload(String json)
+{
     StaticJsonDocument<200> obj;
     DeserializationError error = deserializeJson(obj, json);
-    if(debug && error)Serial.println("Error deserializeJson");
+    if (debug && error)
+        Serial.println("Error deserializeJson");
     String payload = obj["payload"];
     return payload;
+}
+bool iSYNC::mqConnected(){
+    return MQTT->connected();
+}
+bool iSYNC::mqConnect()
+{
+    if (!mqConnected())
+    {
+        if(debug)Serial.print("MQTT connection...");
+        String clientId = "";
+        clientId = "ArduinoClient-" + username + "-" + String(random(0xffff), HEX);
+        
+        if (MQTT->connect(clientId.c_str(), username.c_str(), auth.c_str()))
+        {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+void iSYNC::mqLoop()
+{
+    MQTT->loop();
+}
+void iSYNC::mqInit(String _username, String _auth){
+    mqInit(_username,_auth,false);
+}
+void iSYNC::mqInit(String _username, String _auth,bool secure)
+{
+    if (debug)Serial.print("MQTT Init...");
+    username = _username;
+    auth = _auth;
+    MQTT = new PubSubClient(*clientSecure);
+    if(secure)MQTT->setServer(SERVER_MQTT, 8883);
+    else MQTT->setServer(SERVER_MQTT, 1883);
+    if (debug)Serial.println("Success");
+}
+iSYNC &iSYNC::mqCallback(MQTT_CALLBACK_SIGNATURE)
+{
+    MQTT->setCallback(callback);
+    String clientId = "";
+    clientId = "ArduinoClient-" + username + "-" + String(random(0xffff), HEX);
+    MQTT->connect(clientId.c_str(), username.c_str(), auth.c_str());
+}
+bool iSYNC::mqPub(String key, String msg)
+{
+    if(debug)Serial.println("MQTT Publish -> "+msg);
+    String _topic = "iSYNC/" + auth + "/" + key;
+    return MQTT->publish(_topic.c_str(), msg.c_str());
+}
+bool iSYNC::mqSub(String key)
+{
+    if(debug)Serial.println("MQTT Subscribe key."+key);
+    String _topic = "iSYNC/" + auth + "/" + key;
+    return MQTT->subscribe(_topic.c_str());
+}
+bool iSYNC::mqSubProject()
+{
+    if(debug)Serial.println("MQTT Subscribe project.");
+    String _topic = "iSYNC/" + auth + "/#";
+    return MQTT->subscribe(_topic.c_str());
 }
--- src/iSYNC.h
+++ src/iSYNC.h
@@ -2,38 +2,64 @@
 #define iSYNC_H
 
 #include "Arduino.h"
-#include <ArduinoJson.h>
+#include "lib/ArduinoJson/ArduinoJson.h"
+#include "lib/PubSubClient/PubSubClient.h"
 
 #if defined(ESP8266)
     #include <ESP8266WiFi.h>
     #include <WiFiClientSecure.h>
 #elif defined(ESP32)
+    #include <WiFi.h>
     #include <WiFiClientSecure.h>
 #endif
 
-#define Version "iSYNC library Version 0.1"
-#define SERVER_API "www.isync.pro"
+
+#define Version "\niSYNC library Version 0.2"
+#define SERVER_API "api.isync.pro"
+#define SERVER_MQTT "mq.isync.pro"
+
+#if defined(ESP8266) || defined(ESP32)
+    #include <functional>
+    #define MQTT_CALLBACK_SIGNATURE std::function<void(char*, uint8_t*, unsigned int)> callback
+#else
+    #define MQTT_CALLBACK_SIGNATURE void (*callback)(char*, uint8_t*, unsigned int)
+#endif
 
 class iSYNC
 {
-    public:
-        iSYNC();
-        void debugMode(bool);
-        void begin(String,String);
-
-        String HTTP_GET_RAW(String);
-        String HTTP_POST(String,String);
-
-        String HTTP_GET(String);
-
-        String getPayload(String);
-
-        String getVersion(){return Version;};
-
     private:
         bool debug;
         bool httpRuning;
-        WiFiClientSecure clientSecure;
+        // WiFiClientSecure clientSecure;
+        MQTT_CALLBACK_SIGNATURE;
+        String auth,username;
+        Client *clientSecure;
+    public:
+        iSYNC(Client&);
+        void debugMode(bool);
+        void begin(String,String);  //setup wifi
+
+        /**
+         * HTTP API lib
+         * */
+        String HTTP_GET_RAW(String,String);
+        String HTTP_POST(String,String,String);
+        String HTTP_GET(String,String);
+        String getPayload(String);
+
+        
+        PubSubClient *MQTT;
+        void mqInit(String, String);
+        void mqInit(String, String, bool);
+        iSYNC& mqCallback(MQTT_CALLBACK_SIGNATURE);
+        void mqLoop();
+        bool mqPub(String,String);
+        bool mqSub(String);
+        bool mqSubProject();
+        bool mqConnect();
+        bool mqConnected();
+
+        String getVersion(){return Version;};
 };
 
 #endif
